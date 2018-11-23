0c90101d35cfeff6b46a14c8f3b727891cf686ad
Quella-Langshy <34937351+Quella-Langshy@users.noreply.github.com>
2018-11-23 10:55:17 +0800

    change some pages

--- Project_2/resources/views/layouts/default.blade.php
+++ Project_2/resources/views/layouts/default.blade.php
@@ -6,10 +6,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{--设置视图title--}}
-    <title>@yield('title','Sample')</title>
+    <title>@yield('title','Sample App') - Laravel 入门教学</title>
+    <link rel="stylesheet" href="{{url('/css/app.css')}}">
 </head>
 <body>
     {{--通用视图，子视图通过继承通用视图将内容显示于@yield('content')之中--}}
+    {{--@yield('content')--}}
+
+<header class="navbar navbar-fixed-top navbar-inverse">
+    <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+            <a href="{{url('/')}}" id="logo">Sample App</a>
+            <nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{url('/help')}}">帮助</a></li>
+                    <li><a href="#">登录</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+<div class="container">
     @yield('content')
+</div>
 </body>
 </html>
--- Project_2/resources/views/static_pages/home.blade.php
+++ Project_2/resources/views/static_pages/home.blade.php
@@ -1,6 +1,23 @@
 {{--子视图通过进程父类视图，渲染内容--}}
+{{--@extends('layouts.default')--}}
+{{--@section('title','主页')--}}
+{{--@section('content')--}}
+    {{--<h1>主页</h1>--}}
+{{--@stop--}}
 @extends('layouts.default')
-@section('title','主页')
+
 @section('content')
-    <h1>主页</h1>
-@stop
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是<a href="http://www.chenrq.cc">CRQ-Langshy</a>
+        </p>
+        <p>
+            一切，将从这里开始
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
+
+    @stop
